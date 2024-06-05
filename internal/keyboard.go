bf4d67b7939754a201a76d63f3a24ff10a423c2a
Mario Kozjak <mario.kozjak@elpheria.com>
2024-06-05 21:11:09 +0200

    move global input handler into a separate file
    Signed-off-by: Mario Kozjak <mario.kozjak@elpheria.com>

---
+++ internal/keyboard.go
@@ -0,0 +1,49 @@
+package internal
+
+import "github.com/gdamore/tcell/v2"
+
+func (a *App) KbGlobalHandler(event *tcell.EventKey) *tcell.EventKey {
+	switch event.Key() {
+	case tcell.KeyCtrlQ:
+		a.Application.Stop()
+	case tcell.KeyTab:
+		if !a.AlbumPane.HasFocus() {
+			a.Application.SetFocus(a.AlbumPane)
+			a.ArtistPane.SetSelectedBackgroundColor(tcell.ColorLightGray)
+		} else {
+			a.Application.SetFocus(a.ArtistPane)
+			a.ArtistPane.SetSelectedBackgroundColor(tcell.ColorCornflowerBlue)
+		}
+
+		return nil
+	case tcell.KeyCtrlB:
+		return tcell.NewEventKey(tcell.KeyPgUp, 0, tcell.ModNone)
+	case tcell.KeyCtrlF:
+		return tcell.NewEventKey(tcell.KeyPgDn, 0, tcell.ModNone)
+	}
+
+	switch event.Rune() {
+	case 'p':
+		go a.Playpause()
+	case 's':
+		go a.Stop()
+	case '>':
+		go a.Next()
+	case '<':
+		go a.Previous()
+	case '+':
+		go a.VolumeHold(true)
+	case '-':
+		go a.VolumeHold(false)
+	case 'm':
+		go a.ToggleMute()
+	case 'o':
+		a.ArtistPane.SetCurrentItem(a.CpArtistIdx)
+	case 'u':
+		go a.RefreshData()
+	case 'q':
+		a.Application.Stop()
+	}
+
+	return event
+}
