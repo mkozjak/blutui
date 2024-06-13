2f032727cd3872a082b1930ac29ba8207e573ecf
Mario Kozjak <mario.kozjak@elpheria.com>
2024-06-13 21:49:24 +0200

    add BenchmarkJWSimilarity benchmark
    Signed-off-by: Mario Kozjak <mario.kozjak@elpheria.com>

--- internal/util_test.go
+++ internal/util_test.go
@@ -10,3 +10,15 @@ func TestJWSimilarity(t *testing.T) {
 		t.Error("Expected 0.91898, got ", d)
 	}
 }
+
+var result float64
+
+func BenchmarkJWSimilarity(b *testing.B) {
+	var r float64
+
+	for n := 0; n < b.N; n++ {
+		r = JWSimilarity("faremviel", "farmville")
+	}
+
+	result = r
+}
