dc575a1197ee0f4e0aad06ff5f08a10fb3717253
Mario Kozjak <mario.kozjak@elpheria.com>
2024-06-25 14:59:17 +0200

    util unit tests done
    Signed-off-by: Mario Kozjak <mario.kozjak@elpheria.com>

--- internal/util_test.go
+++ internal/util_test.go
@@ -5,6 +5,125 @@ import (
 	"testing"
 )
 
+func TestFormatDuration(t *testing.T) {
+	type test struct {
+		sec  int
+		want string
+	}
+
+	tests := []test{
+		{sec: 120, want: "02:00"},
+		{sec: 34, want: "00:34"},
+		{sec: 0, want: "00:00"},
+	}
+
+	for _, tc := range tests {
+		got := FormatDuration(tc.sec)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Fatalf("expected: %v, got: %v", tc.want, got)
+		}
+	}
+}
+
+func TestCapitalizeArtist(t *testing.T) {
+	type test struct {
+		s    string
+		want string
+	}
+
+	tests := []test{
+		{s: "koRn", want: "KoRn"},
+		{s: "", want: ""},
+		{s: "camel", want: "Camel"},
+		{s: "(foo", want: "(foo"},
+	}
+
+	for _, tc := range tests {
+		got := CapitalizeArtist(tc.s)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Fatalf("expected: %v, got: %v", tc.want, got)
+		}
+	}
+}
+
+func TestCaser(t *testing.T) {
+	type test struct {
+		s    string
+		want string
+	}
+
+	tests := []test{
+		{s: "koRn", want: "Korn"},
+		{s: "croatia", want: "Croatia"},
+		{s: "mercedes", want: "Mercedes"},
+	}
+
+	for _, tc := range tests {
+		got := Caser(tc.s)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Fatalf("expected: %v, got: %v", tc.want, got)
+		}
+	}
+}
+
+func TestExtractAlbumYear(t *testing.T) {
+	type ok struct {
+		y    string
+		want int
+	}
+
+	type nok struct {
+		y    string
+		want string
+	}
+
+	oks := []ok{
+		{y: "2023", want: 2023},
+		{y: "2023-12-31", want: 2023},
+		{y: "0", want: 0},
+	}
+
+	for _, tc := range oks {
+		got, _ := ExtractAlbumYear(tc.y)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Fatalf("expected: %v, got: %v", tc.want, got)
+		}
+	}
+
+	fails := []nok{
+		{y: "2x23", want: "invalid date format"},
+		{y: "2000-", want: "invalid date format"},
+	}
+
+	for _, tc := range fails {
+		_, err := ExtractAlbumYear(tc.y)
+		if !reflect.DeepEqual(err.Error(), tc.want) {
+			t.Errorf("expected: %v, got: %v", tc.want, err.Error())
+		}
+	}
+
+}
+
+func TestCleanTrackName(t *testing.T) {
+	type test struct {
+		s    string
+		want string
+	}
+
+	tests := []test{
+		{s: "01. What Is This", want: "What Is This"},
+		{s: "Make Me Bad", want: "Make Me Bad"},
+		{s: "mercedes", want: "mercedes"},
+	}
+
+	for _, tc := range tests {
+		got := CleanTrackName(tc.s)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Fatalf("expected: %v, got: %v", tc.want, got)
+		}
+	}
+}
+
 func TestJWSimilarity(t *testing.T) {
 	type test struct {
 		s1   string
